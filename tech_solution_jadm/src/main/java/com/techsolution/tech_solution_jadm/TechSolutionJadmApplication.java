d374efb4c127e201f9026203d9791bc6ebef06eb
JAD128 <juandima4@gmail.com>
2026-04-15 07:43:10 -0500

    punto 1

---
+++ tech_solution_jadm/src/main/java/com/techsolution/tech_solution_jadm/TechSolutionJadmApplication.java
@@ -0,0 +1,13 @@
+package com.techsolution.tech_solution_jadm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechSolutionJadmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechSolutionJadmApplication.class, args);
+	}
+
+}
---
+++ tech_solution_jadm/src/test/java/com/techsolution/tech_solution_jadm/TechSolutionJadmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techsolution.tech_solution_jadm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechSolutionJadmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
